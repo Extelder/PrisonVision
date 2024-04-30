f0a6da22d9057ccf992b99d8d21b64309bd562d0
igoryava <126491702+igoryava@users.noreply.github.com>
2024-04-30 14:18:31 +0300

    Added SOME PASHALKO USELESS

--- Assets/Scripts/EnemyAI/GuardianPatrollerAI.cs
+++ Assets/Scripts/EnemyAI/GuardianPatrollerAI.cs
@@ -1,25 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GuardianPatrollerAI : MonoBehaviour
 {
     [SerializeField] private float _movementSpeed;
     [SerializeField] private Transform[] _waypoints;
-
-    //[SerializeField] private Animator _animator;
+    [SerializeField] private Animator _animator;
 
     private int _currentWaypoint = 0;
-
     private Rigidbody _rigidbody;
 
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
-        //_animator = GetComponent<Animator>();
+        _animator = GetComponent<Animator>();
     }
 
     private void Update()
+    {
+        MoveToWaypoint();
+        RotateTowardsWaypoint();
+    }
+
+    private void MoveToWaypoint()
     {
         if (Vector3.Distance(transform.position, _waypoints[_currentWaypoint].position) <= 1)
         {
@@ -34,6 +36,13 @@ public class GuardianPatrollerAI : MonoBehaviour
         }
 
         transform.position = Vector3.MoveTowards(transform.position, _waypoints[_currentWaypoint].position, _movementSpeed * Time.deltaTime);
+        _animator.SetBool(name: "isWalk", value: (Vector3.Distance(transform.position, _waypoints[_currentWaypoint].position) <= 1));
+    }
 
+    private void RotateTowardsWaypoint()
+    {
+        Vector3 directionToWaypoint = (_waypoints[_currentWaypoint].position - transform.position).normalized;
+        Quaternion targetRotation = Quaternion.LookRotation(directionToWaypoint);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, _movementSpeed * Time.deltaTime);
     }
 }
